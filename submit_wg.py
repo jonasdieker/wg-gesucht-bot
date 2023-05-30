7881ce1b4230153ac2367b075edf97dfe8cdbc51
jonasdieker <jonasdieker05@gmail.com>
2023-05-30 21:09:46 +0200

    further refactor of selenium code

--- submit_wg.py
+++ submit_wg.py
@@ -4,12 +4,11 @@ import random
 import time
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
 
 def get_login_credentials():
     with open("login-creds.json", "r") as f:
@@ -23,19 +22,36 @@ def get_random_wait_time():
 def get_element(driver, by, id):
     try:
         element = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((by, id))
+            EC.visibility_of_element_located((by, id))
         )
-    except NoSuchElementException or TimeoutException:
+    except NoSuchElementException:
         driver.quit()
-        return None
+        raise NoSuchElementException()
+    except TimeoutException:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((by, id)))
     if isinstance(element, list):
         element = element[0]
     return element
 
 
+def click_button(driver, by, id):
+    try:
+        element = get_element(driver, by, id)
+        element.click()
+    except ElementNotInteractableException:
+        raise ElementNotInteractableException()
+        
+
+def send_keys(driver, by, id, send_str):
+    try:
+        element = get_element(driver, by, id)
+        element.send_keys(send_str)
+    except ElementNotInteractableException:
+        raise ElementNotInteractableException(f"Could not enter: {send_str}")
+
+
 def submit_app(ref, logger):
-    # change the location of the driver on your machine
-    # create ChromeOptions object
+
     chrome_options = webdriver.ChromeOptions()
     creds = get_login_credentials()
 
@@ -49,32 +65,36 @@ def submit_app(ref, logger):
     # chrome_options.add_argument("--headless")
 
     # create the ChromeDriver object
-    driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=chrome_options)
-    driver.get("https://www.wg-gesucht.de/nachricht-senden/" + ref)
+    try:
+        service_log_path = "chromedriver.log"
+        service_args = ["--verbose"]
+        driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options, service_args=service_args, service_log_path=service_log_path)
+        driver.get("https://www.wg-gesucht.de/nachricht-senden/" + ref)
+    except:
+        logger.log("Chrome crashed! You might be trying to run it without a screen in terminal? Look at 'chromedriver.log'.")
+        driver.quit()
+
+    # mainly when using screen
     driver.maximize_window()
 
-    accept_button = get_element(driver, By.XPATH, "//*[contains(text(), 'Accept all')]")
-    if accept_button:
-        accept_button.click()
+    # accept cookies button
+    click_button(driver, By.XPATH, "//*[contains(text(), 'Accept all')]")
 
-    konto_button = get_element(driver, By.XPATH, "//*[contains(text(), 'Mein Konto')]")
-    if konto_button:
-        konto_button.click()
+    # my account button
+    click_button(driver, By.XPATH, "//*[contains(text(), 'Mein Konto')]")
 
     time.sleep(2)
 
-    email = get_element(driver, By.ID, "login_email_username")
-    if email:
-        email.send_keys(creds["email"])
+    # enter email
+    send_keys(driver, By.ID, "login_email_username", creds["email"])
 
-    passwd = get_element(driver, By.ID, "login_password")
-    if passwd:
-        passwd.send_keys(creds["password"])
+    # enter password
+    send_keys(driver, By.ID, "login_password", creds["password"])
 
-    login_button1 = get_element(driver, By.ID, "login_submit")
-    if login_button1:
-        login_button1.click()
+    # login button
+    click_button(driver, By.ID, "login_submit")
 
+    # occasionally wg-gesucht gives you advice on how to stay safe.
     try:
         se_button1 = get_element(driver, By.ID, "sicherheit_bestaetigung")
         se_button1.click()
@@ -83,7 +103,6 @@ def submit_app(ref, logger):
 
     # checks if already sent message to flat posting.
     try:
-        # driver.find_element("id", "message_timestamp")
         _ = get_element(driver, By.ID, "message_timestamp")
         logger.info("Message has already been sent previously. Will skip this offer.")
         driver.quit()
@@ -106,15 +125,16 @@ def submit_app(ref, logger):
         logger.info("message.txt file not found!")
         return 0
 
+    time.sleep(2)
+
     try:
-        submit_button = get_element(
-            driver,
+        click_button(driver,
             By.XPATH,
             "//button[@data-ng-click='submit()' or contains(.,'Nachricht senden')]",
-        )
-        submit_button.click()
+            )
         logger.info(f">>>> Message sent to: {ref} <<<<")
-    except NoSuchElementException:
+    except ElementNotInteractableException:
         logger.info("Cannot find submit button!")
         driver.quit()
+    time.sleep(1)
     driver.quit()
